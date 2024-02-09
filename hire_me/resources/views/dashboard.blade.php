61e514d2dd70b360fc4262f36ef75b398d743dcd
RADIA IDELKADI <idelkadiradia@gmail.com>
2024-02-09 18:23:50 +0100

    front company*

--- hire_me/resources/views/dashboard.blade.php
+++ hire_me/resources/views/dashboard.blade.php
@@ -1,17 +1,123 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <div class="max-w-4xl mx-auto p-8">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+        <!-- Bouton d'ajout d'offre d'emploi -->
+        <button onclick="showModal()" id="showModalButton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-8 right-0" >Ajouter une offre d'emploi</button>
+
+        <!-- Liste des offres d'emploi -->
+        <div class="space-y-4">
+            <!-- Première carte -->
+            <div class="bg-white rounded-lg shadow-md overflow-hidden">
+                <div class="flex p-4">
+                    <img class="w-16 h-16 rounded-full mr-4" src="https://civi.uxper.co/wp-content/uploads/2022/11/avatar_uxper.png" alt="Logo de l'entreprise">
+                    <div>
+                        <h3 class="font-semibold text-lg"><a href="https://civi.uxper.co/jobs/development-it/sr-backend-go-developer-crypto-industry/">Sr. Backend Go Developer</a></h3>
+                        <p class="text-gray-600">par <a href="https://civi.uxper.co/companies/software/uxper/" class="text-blue-500">Uxper</a> dans <a href="https://civi.uxper.co/jobs-categories/development-it/" class="text-blue-500">Development &amp; IT</a></p>
+                    </div>
+                </div>
+                <div class="p-4">
+                    <div class="flex flex-wrap gap-2 mb-2">
+                        <span class="bg-blue-100 text-blue-500 px-2 py-1 rounded-full text-xs border border-blue-500">Remote</span>
+                        <span class="bg-blue-100 text-blue-500 px-2 py-1 rounded-full text-xs border border-blue-500">Ashkasham</span>
+                    </div>
+                </div>
+                <div class="bg-gray-100 p-4 flex justify-between items-center">
+                    <div class="mr-4">
+                        <p class="text-gray-600">571 days left to apply</p>
+                    </div>
+                    <div>
+                        <p class="text-gray-600">$1000 - $2000/month</p>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <!-- Modal d'ajout d'offre d'emploi -->
+        <div id="myModal" class="hidden fixed inset-0 overflow-y-auto" x-show="showModal" @click.away="showModal = false">
+            <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <!-- Overlay -->
+                <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                    <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                </div>
+
+                <!-- Contenu du modal -->
+                <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                    <!-- Contenu du modal ici -->
+                    <div class="flex justify-end px-4 py-2">
+                        <button onclick="hideModal()" class="text-gray-500 hover:text-gray-700 focus:outline-none">
+                            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <div class="sm:flex sm:items-start">
+                            <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                                <!-- Icône d'ajout -->
+                                <svg class="h-6 w-6 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
+                                </svg>
+                            </div>
+                            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">Ajouter une nouvelle offre d'emploi</h3>
+                                <div>
+                                    <form action="/ajouter-offre" method="POST">
+                                        @csrf 
+                                        <div class="mb-4">
+                                            <label for="titre" class="block text-gray-700 font-bold mb-2">Titre de l'offre :</label>
+                                            <input type="text" id="titre" name="titre" class="border border-gray-300 rounded-md px-3 py-2 w-full" placeholder="Titre de l'offre" required>
+                                        </div>
+                                        <div class="mb-4">
+                                            <label for="description" class="block text-gray-700 font-bold mb-2">Description :</label>
+                                            <textarea id="description" name="description" class="border border-gray-300 rounded-md px-3 py-2 w-full" placeholder="Description de l'offre" required></textarea>
+                                        </div>
+                                        <div class="mb-4">
+                                            <label for="competences" class="block text-gray-700 font-bold mb-2">Compétences requises :</label>
+                                            <textarea type="text" id="competences" name="competences" class="border border-gray-300 rounded-md px-3 py-2 w-full" placeholder="Compétences requises" required></textarea>
+                                        </div>
+                                        <div class="mb-4">
+                                            <label for="type_contrat" class="block text-gray-700 font-bold mb-2">Type de contrat :</label>
+                                            <select id="type_contrat" name="type_contrat" class="border border-gray-300 rounded-md px-3 py-2 w-full" required>
+                                                <option value="distance">À distance</option>
+                                                <option value="hybride">Hybride</option>
+                                                <option value="temps_plein">À temps plein</option>
+                                            </select>
+                                        </div>
+                                        <div class="mb-4">
+                                            <label for="emplacement" class="block text-gray-700 font-bold mb-2">Emplacement :</label>
+                                            <input type="text" id="emplacement" name="emplacement" class="border border-gray-300 rounded-md px-3 py-2 w-full" placeholder="Emplacement" required>
+                                        </div>
+                                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Ajouter</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 </x-app-layout>
+
+<script>
+    // Récupérer le bouton d'ajout d'offre d'emploi
+    const showModalButton = document.getElementById('showModalButton');
+    // Récupérer le modal
+    const modal = document.getElementById('myModal');
+
+    // Fonction pour afficher le modal
+    function showModal() {
+        modal.classList.remove('hidden');
+    }
+
+    // Fonction pour masquer le modal
+    function hideModal() {
+        modal.classList.add('hidden');
+    }
+
+    // Écouter l'événement de clic sur le bouton
+    showModalButton.addEventListener('click', showModal);
+</script>
--- hire_me/resources/views/layouts/navigation.blade.php
+++ hire_me/resources/views/layouts/navigation.blade.php
@@ -6,15 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block fill-current text-gray-800" />
                     </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
                 </div>
             </div>
 
@@ -34,10 +27,12 @@
                     </x-slot>
 
                     <x-slot name="content">
+                        <x-dropdown-link :href="route('dashboard')">
+                            {{ __('Offers') }}
+                        </x-dropdown-link>
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
-
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
